d119aeacff4b9a2e4539c07b480307329ec4d3a3
Eragon <hnguyenngoc.h@gmail.com>
2025-04-15 23:58:02 +0700

    Add: create BaseSevice and Brand Interface

---
+++ Structure_Base/BaseService/ICRUD_Service.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure_Base.BaseService;
+public interface ICRUD_Service<T, U>
+{
+    //Task<ResultService<T>> Create(T entity);
+    //Task<ResultService<T>> Update(T entity);  
+    Task<ResultService<string>> Delete(U id);
+    Task<ResultService<T>> Get(U id);
+    Task<ResultService<IEnumerable<T>>> GetAll();
+}
---
+++ Structure_Base/IBrandProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure_Base;
+public interface IBrandProvider
+{
+    Task<ResultService<Brand>> SaveByDapper(Brand entity);
+    Task<ResultService<Brand>> GetByCode(string brandCode);
+    Task<ResultService<string>> DeleteByDapper(string brandCode);
+}
